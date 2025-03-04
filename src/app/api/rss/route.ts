434c461ca28a19fc34f56adfc988cad33c6e17d7
Austin Chiatto <austin@forgeandsmith.com>
2025-03-03 17:30:51 -0800

    update: test output formatting

--- src/app/api/rss/route.ts
+++ src/app/api/rss/route.ts
@@ -37,9 +37,8 @@ export async function GET() {
       const providerDetails = getProviderDetails(mission.launch_service_provider);
 
       const customDescription = `
-			<p>${missionDetails.statusName}</p>
-			<p><strong>Launch time</strong>: ${missionDetails.net}</p>
-			</br>
+			<p><strong>Launch Status</strong>: ${missionDetails.statusName}</p>
+			<p><strong>Launch Time</strong>: ${missionDetails.net}</p>
 			</br>
 			<h2>Mission Details</h2>
 			<p><strong>Mission Type</strong>: ${missionDetails.type}</p>
@@ -48,33 +47,46 @@ export async function GET() {
 			<p><strong>Launch site</strong>: ${missionDetails.padName}</p>
 			<p>${missionDetails.desc}</p>
 			</br>
-			</hr>
-			</br>
-			<h2>Launch Vehicle</h2>
-			<p><strong>Configuration</strong>: ${rocketDetails.fullName}</p>
+			<h2>${rocketDetails.fullName}</h2>
 			<p>${rocketDetails.desc}</p>
-			</br>
-			<h3>Stats</h3>
-			<p><strong>Length</strong>: ${rocketDetails.length ?? 'N/A'} m</p>
-			<p><strong>Diameter</strong>: ${rocketDetails.diameter ?? 'N/A'} m</p>
-			<p><strong>Launch Mass</strong>: ${rocketDetails.launchCost ?? 'N/A'}</p>
-			<p><strong>LEO Capacity</strong>: ${rocketDetails.capacityLeo ?? 'N/A'}</p>
-			<p><strong>GTO Capacity</strong>: ${rocketDetails.capacityGto ?? 'N/A'}</p>
-			<p><strong>Thrust</strong>: ${rocketDetails.thrustTo ?? 'N/A'} kN</p>
-			</br>
-			</hr>
+			<ul>
+				<li><strong>Length</strong>: ${rocketDetails.length ?? 'N/A'} m</li>
+				<li><strong>Diameter</strong>: ${rocketDetails.diameter ?? 'N/A'} m</li>
+				<li><strong>Launch Mass</strong>: ${rocketDetails.launchCost ?? 'N/A'}</li>
+				<li><strong>LEO Capacity</strong>: ${rocketDetails.capacityLeo ?? 'N/A'}</li>
+				<li><strong>GTO Capacity</strong>: ${rocketDetails.capacityGto ?? 'N/A'}</li>
+				<li><strong>Thrust</strong>: ${rocketDetails.thrustTo ?? 'N/A'} kN</li>
+			</ul>
+
+			<h3>${rocketDetails.fullName} Launch Record</h3>
+			<ul>
+				<li><strong>Successful Launches</strong>: ${rocketDetails.launchSuccessCount ?? 'N/A'}</li>
+				<li><strong>Failed Launches</strong>: ${rocketDetails.launchFailedCount ?? 'N/A'}</li>
+				<li><strong>Successful Landings</strong>: ${rocketDetails.landingSuccessCount ?? 'N/A'}</li>
+				<li><strong>Failed Landings</strong>: ${rocketDetails.landingFailedCount ?? 'N/A'}</li>
+			</ul>
+
 			</br>
 			<h2>${providerDetails.name}</h2>
-			<p>${providerDetails.desc}</p>
 			<p><strong>Administrator</strong>: ${providerDetails.administrator}</p>
 			<p><strong>Type</strong>: ${providerDetails.type}</p>
 			<p><strong>Founding Year</strong>: ${providerDetails.foundingYear}</p>
+			</br>
+			<p>${providerDetails.desc}</p>
+
+			<h3>${providerDetails.name} Launch Record</h3>
+			<ul>
+				<li><strong>Successful Launches</strong>: ${providerDetails.launchSuccessCount ?? 'N/A'}</li>
+				<li><strong>Failed Launches</strong>: ${providerDetails.launchFailedCount ?? 'N/A'}</li>
+				<li><strong>Successful Landings</strong>: ${providerDetails.landingSuccessCount ?? 'N/A'}</li>
+				<li><strong>Failed Landings</strong>: ${providerDetails.landingFailedCount ?? 'N/A'}</li>
+			</ul>
   `;
 
       feed.item({
         title: mission.name,
         description: customDescription || 'No mission description provided',
-        url: mission.infographic || 'http://localhost:3000',
+        url: missionDetails.image,
         date: mission.net || new Date().toString()
       });
     });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="">
-      <main className="">test</main>
+      <main className=""></main>
     </div>
   );
 }
