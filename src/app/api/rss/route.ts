69540be94e5b67fe3261e7f8e03e6973d83c70e0
Austin Chiatto <austin@forgeandsmith.com>
2025-03-03 18:43:27 -0800

    update: testing output

--- src/app/api/rss/route.ts
+++ src/app/api/rss/route.ts
@@ -47,50 +47,44 @@ export async function GET() {
       const customDescription = `
 			<img src="${mission.image}" alt="Launch image" style="max-width:100%; height:auto;" />
 			<a href="${mission.url}">Watch Now</a>
-			<h1>${mission.name}</h1>
 			</br>
 			<p><strong>Launch Status</strong>: ${missionDetails.statusName}</p>
 			<p><strong>Launch Time</strong>: ${missionDetails.net}</p>
+			</br>
 			<h2>Mission Details</h2>
+			<p><strong>Mission Type</strong> - ${missionDetails.type}
+			</br><strong>Mission Destination</strong> - ${missionDetails.orbitName}
+			</br><strong>Program</strong> - (Add program info if needed)
+			</br><strong>Launch site</strong> - ${missionDetails.padName}</p>
 			<p>${truncate(missionDetails.desc, 310)}</p>
-				<p><strong>Mission Type</strong> - ${missionDetails.type}
-				</br><strong>Mission Destination</strong> - ${missionDetails.orbitName}
-				</br><strong>Program</strong> - (Add program info if needed)
-				</br><strong>Launch site</strong> - ${missionDetails.padName}</p>
 			</br>
 			<h2>${rocketDetails.fullName}</h2>
 			<p>${truncate(rocketDetails.desc, 310)}</p>
 			<a href="${rocketDetails.info_url}">Read More</a>
 			<h5>Vehicle Stats</h5>
-			<ul>
-				<li><strong>Length</strong> - ${rocketDetails.length ?? 'N/A'} m</li>
-				<li><strong>Diameter</strong> - ${rocketDetails.diameter ?? 'N/A'} m</li>
-				<li><strong>Launch Mass</strong> - ${rocketDetails.launchCost ?? 'N/A'}</li>
-				<li><strong>LEO Capacity</strong> - ${rocketDetails.capacityLeo ?? 'N/A'}</li>
-				<li><strong>GTO Capacity</strong> - ${rocketDetails.capacityGto ?? 'N/A'}</li>
-				<li><strong>Thrust</strong> - ${rocketDetails.thrustTo ?? 'N/A'} kN</li>
-			</ul>
+			<p><strong>Length</strong> - ${rocketDetails.length ?? 'N/A'} m
+			</br><strong>Diameter</strong> - ${rocketDetails.diameter ?? 'N/A'} m
+			</br><strong>Launch Mass</strong> - ${rocketDetails.launchCost ?? 'N/A'}
+			</br><strong>LEO Capacity</strong> - ${rocketDetails.capacityLeo ?? 'N/A'}
+			</br><strong>GTO Capacity</strong> - ${rocketDetails.capacityGto ?? 'N/A'}
+			</br><strong>Thrust</strong> - ${rocketDetails.thrustTo ?? 'N/A'} kN</p>
 			<h5>Launch & Landing Record</h5>
-			<ul>
-				<li><strong>Successful Launches</strong> - ${rocketDetails.launchSuccessCount ?? 'N/A'}</li>
-				<li><strong>Failed Launches</strong> - ${rocketDetails.launchFailedCount ?? 'N/A'}</li>
-				<li><strong>Successful Landings</strong> - ${rocketDetails.landingSuccessCount ?? 'N/A'}</li>
-				<li><strong>Failed Landings</strong> - ${rocketDetails.landingFailedCount ?? 'N/A'}</li>
-			</ul>
+			<p><strong>Successful Launches</strong> - ${rocketDetails.launchSuccessCount ?? 'N/A'}
+			</br><strong>Failed Launches</strong> - ${rocketDetails.launchFailedCount ?? 'N/A'}
+			</br><strong>Successful Landings</strong> - ${rocketDetails.landingSuccessCount ?? 'N/A'}
+			</br><strong>Failed Landings</strong> - ${rocketDetails.landingFailedCount ?? 'N/A'}</p>
 			</br>
 			<h2>${providerDetails.name}</h2>
-			<p><strong>Administrator</strong> - ${providerDetails.administrator}</p>
-			<p><strong>Type</strong> - ${providerDetails.type}</p>
-			<p><strong>Founding Year</strong> - ${providerDetails.foundingYear}</p>
+			<p><strong>Administrator</strong> - ${providerDetails.administrator}
+			</br><strong>Type</strong> - ${providerDetails.type}
+			</br><strong>Founding Year</strong> - ${providerDetails.foundingYear}</p>
 			<p>${truncate(providerDetails.desc, 310)}</p>
 			<a href="${providerDetails.info_url}">Read More</a>
 			<h5>Launch & Landing Record</h5>
-			<ul>
-				<li><strong>Successful Launches</strong> - ${providerDetails.launchSuccessCount ?? 'N/A'}</li>
-				<li><strong>Failed Launches</strong> - ${providerDetails.launchFailedCount ?? 'N/A'}</li>
-				<li><strong>Successful Landings</strong> - ${providerDetails.landingSuccessCount ?? 'N/A'}</li>
-				<li><strong>Failed Landings</strong> - ${providerDetails.landingFailedCount ?? 'N/A'}</li>
-			</ul>
+			<p><strong>Successful Launches</strong> - ${providerDetails.launchSuccessCount ?? 'N/A'}
+			</br><strong>Failed Launches</strong> - ${providerDetails.launchFailedCount ?? 'N/A'}
+			</br><strong>Successful Landings</strong> - ${providerDetails.landingSuccessCount ?? 'N/A'}
+			</br><strong>Failed Landings</strong> - ${providerDetails.landingFailedCount ?? 'N/A'}</p>
   `;
 
       feed.item({
